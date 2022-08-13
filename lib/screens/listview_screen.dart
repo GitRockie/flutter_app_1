4382a71752452647bbce99b05e5b40f017ca7a00
Roman Kostyuchenko <elromankos@gmail.com>
2022-08-13 23:55:43 +0200

    ListTile from Static List
    List tile in children
    Icon add
    Map for Iterable
    operator spread ...for every element extract from List to demostrate the widget
    trailing implement

--- lib/screens/listview_screen.dart
+++ lib/screens/listview_screen.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Listview1Screen extends StatelessWidget {
+  final options = const [
+    'Roman Kostyuchenko',
+    'Sergi Kostyuchenko',
+    'Mauricio Jauregui Lauffer ',
+    'Secot',
+    'Meri Miró'
+  ];
+
   const Listview1Screen({Key? key}) : super(key: key);
 
   @override
@@ -10,12 +18,17 @@ class Listview1Screen extends StatelessWidget {
           title: const Text('Listview1 Type 1'),
         ),
         body: ListView(
-          children: const [
-            Text('Hola Mundo'),
-            Text('Hola Mundo'),
-            Text('Hola Mundo'),
-            Text('Hola Mundo'),
-            Text('Hola Mundo'),
+          children: [
+            ...options
+                .map((team) => ListTile(
+                      title: Text(team),
+                      trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                    ))
+                .toList()
+
+            //ListTile(
+            //title: Text('Our Team'),
+            //),
           ],
         ));
   }
