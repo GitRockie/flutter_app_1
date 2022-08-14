f3a7ba85af268680868186a2a962f8c3fff50b8f
Roman Kostyuchenko <elromankos@gmail.com>
2022-08-14 16:50:48 +0200

    Initial root and HomeScreen
    AlertScreen create
    CardScreen create
    Export index create
    Export in index screens
    InitialRoute set

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_app_1/screens/listview2_screen.dart';
+import 'package:flutter_app_1/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,9 +9,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
-        home: Listview2Screen());
+        //home: const CardScreen(),
+        initialRoute: 'home',
+        routes: {
+          'home': (BuildContext context) => const HomeScreen(),
+          'listview1': (BuildContext context) => const Listview1Screen(),
+          'listview2': (BuildContext context) => const Listview2Screen(),
+          'alert': (BuildContext context) => const AlertScreen(),
+          'card': (BuildContext context) => const CardScreen(),
+        });
   }
 }
