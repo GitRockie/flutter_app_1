b34ccd34e8154f193c46e48c77ea2a22c337cab2
Roman Kostyuchenko <elromankos@gmail.com>
2022-08-14 17:04:17 +0200

    HomeScreen Design
    appBar create
    Color set (TODO global)
    onTap navigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,9 +5,19 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Components'),
+        elevation: 0,
+      ),
+      body: ListView.separated(
+        itemCount: 10,
+        itemBuilder: (context, index) => ListTile(
+          leading: const Icon(Icons.access_time_outlined),
+          title: const Text('Root name'),
+          onTap: () {},
+        ),
+        separatorBuilder: (_, __) => const Divider(),
       ),
     );
   }
