f9ef55ccdd3b1781996dd694e65be764ab44af5a
Roman Kostyuchenko <elromankos@gmail.com>
2022-08-15 22:34:47 +0200

    Card Widget creation
    Card create working with List
    Re-use the Card to optimize CardScreen
    CustomCard() creation pending

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_app_1/theme/app_theme.dart';
+
 class CardScreen extends StatelessWidget {
   const CardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CardScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Cards'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          children: [
+            Card(
+              child: Column(
+                children: const [
+                  ListTile(
+                    leading: Icon(
+                      Icons.photo_album_outlined,
+                      color: AppTheme.primary,
+                    ),
+                    title: Text('I am the Title'),
+                    subtitle: Text(
+                        'This card shows the best choice ofeered to you. You got it Officia veniam duis elit duis magna. Occaecat ex consectetur duis eiusmod exercitation id cillum consequat proident laborum qui. Officia et ea voluptate cupidatat tempor qui.'),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
