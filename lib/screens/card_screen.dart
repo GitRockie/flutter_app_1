9ba6983094f5533a65b478adf2f46d2414b9ccbd
Roman Kostyuchenko <elromankos@gmail.com>
2022-08-19 23:20:44 +0200

    Arguments for Widget Image

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -18,19 +18,31 @@ class CardScreen extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-            CustomCardType2(),
+            CustomCardType2(
+              imageUrl:
+                  'https://images.unsplash.com/photo-1537824598505-99ee03483384?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+            ),
             SizedBox(
               height: 10,
             ),
-            CustomCardType2(),
+            CustomCardType2(
+              imageUrl:
+                  ' https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+            ),
             SizedBox(
               height: 10,
             ),
-            CustomCardType2(),
+            CustomCardType2(
+              imageUrl:
+                  'https://images.unsplash.com/photo-1617634667039-8e4cb277ab46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGxhbmRzY2FwZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
+            ),
             SizedBox(
               height: 10,
             ),
-            CustomCardType2(),
+            CustomCardType2(
+              imageUrl:
+                  'https://images.unsplash.com/photo-1614586125858-e695dd97d1b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxhbmRzY2FwZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
+            ),
             SizedBox(
               height: 100,
             ),
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app_1/theme/app_theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({Key? key}) : super(key: key);
+  final String imageUrl;
+
+  const CustomCardType2({Key? key, required this.imageUrl}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,14 +14,13 @@ class CustomCardType2 extends StatelessWidget {
       elevation: 10,
       shadowColor: AppTheme.primary.withOpacity(0.5),
       child: Column(children: [
-        const FadeInImage(
-          image: NetworkImage(
-              'https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=975&q=80'),
-          placeholder: AssetImage('assets/jar-loading.gif'),
+        FadeInImage(
+          image: NetworkImage(imageUrl),
+          placeholder: const AssetImage('assets/jar-loading.gif'),
           width: double.infinity,
           height: 230,
           fit: BoxFit.cover,
-          fadeInDuration: Duration(milliseconds: 300),
+          fadeInDuration: const Duration(milliseconds: 300),
         ),
         Container(
             alignment: AlignmentDirectional.centerEnd,
