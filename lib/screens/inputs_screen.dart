58fc7a83f79d77c29e4b9050ee4234e40abbf818
Roman Kostyuchenko <elromankos@gmail.com>
2022-08-21 15:27:37 +0200

    Centralize Inputs
    Apptheme Inputs create
    CustomInputField extract in Widget
    Export widget

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_input_field.dart';
+
 class InputsScreen extends StatelessWidget {
   const InputsScreen({Key? key}) : super(key: key);
 
@@ -12,36 +14,10 @@ class InputsScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Column(children: [
-            TextFormField(
-              autofocus: false,
-              initialValue: '',
-              textCapitalization: TextCapitalization.words,
-              onChanged: (value) {
-                print(value);
-              },
-              validator: (value) {
-                if (value == null) return 'This camp is required';
-                return value.length < 3 ? '3 characters min. limit' : null;
-              },
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              decoration: const InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.teal),
-                ),
-                hintText: 'Username',
-                labelText: 'Name',
-                helperText: 'Only letters',
-                //counterText: '3 characters'
-                //prefixIcon: Icon(),
-                suffixIcon: Icon(Icons.group_outlined),
-                icon: Icon(Icons.assignment_outlined),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
-                )),
-              ),
+          child: Column(children: const [
+            CustomInputField(
+              labelText: 'Name',
+              hintText: 'Username',
             )
           ]),
         ),
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,3 @@
-export 'package:flutter_app_1/widgets/custom_card_type_2.dart';
-
 export 'package:flutter_app_1/widgets/custom_card_type_1.dart';
+export 'package:flutter_app_1/widgets/custom_card_type_2.dart';
+export 'package:flutter_app_1/widgets/custom_input_field.dart';
