88a4e489fb0062a6e7f91dba3c4009dda2ca3a79
Roman Kostyuchenko <elromankos@gmail.com>
2022-08-21 14:44:24 +0200

    Forms personalize
    Working with decoration
    Change color whe selected

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -11,15 +11,37 @@ class InputsScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
           child: Column(children: [
             TextFormField(
-              autofocus: true,
-              initialValue: 'Simon Baker',
+              autofocus: false,
+              initialValue: '',
               textCapitalization: TextCapitalization.words,
               onChanged: (value) {
                 print(value);
               },
+              validator: (value) {
+                if (value == null) return 'This camp is required';
+                return value.length < 3 ? '3 characters min. limit' : null;
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              decoration: const InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.teal),
+                ),
+                hintText: 'Username',
+                labelText: 'Name',
+                helperText: 'Only letters',
+                //counterText: '3 characters'
+                //prefixIcon: Icon(),
+                suffixIcon: Icon(Icons.group_outlined),
+                icon: Icon(Icons.assignment_outlined),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                )),
+              ),
             )
           ]),
         ),
