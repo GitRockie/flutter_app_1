cb2cd57f5e9540842f0265938a552afe4fa98131
Roman Kostyuchenko <elromankos@gmail.com>
2022-08-23 18:36:22 +0200

    Infinite Scroll
    listening for Listview changes
    Controll of the position of the screen
    ScrollController final create
    BouncingScrollPhysics
    add elements to List
    Create Array 1-5 transform elemants last +1
    setState to upgrade Listview changes
    setState(() {});

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -54,6 +54,39 @@ class AppTheme {
       color: primary,
       elevation: 0,
     ),
+
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(primary: primary),
+    ),
+    //floatingActionButtons
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      elevation: 0,
+      backgroundColor: primary,
+    ),
+
+    //Elevated Buttons
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+          primary: primary, shape: const StadiumBorder(), elevation: 0),
+    ),
+
+    //Inputs
+    inputDecorationTheme: const InputDecorationTheme(
+        floatingLabelStyle: TextStyle(color: primary),
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: primary),
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                topRight: Radius.circular(10))),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: primary),
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                topRight: Radius.circular(10))),
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                topRight: Radius.circular(10)))),
     scaffoldBackgroundColor: Colors.grey[850],
   );
 }
